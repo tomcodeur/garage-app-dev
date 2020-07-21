a5f90ed0e8681129f9577da632941f1b8f1cd387
tomcodeur <mercier.thomas@tomcodeur.com>
2020-07-21 13:59:28 +0200

    Ajout d'élements dans le formulaire

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ export default class App extends Component {
 
   state = {
     checked: true, //Valeur des cases à cocher
+    notChecked: false,
   }
   
   seeAlert() {
@@ -33,6 +34,10 @@ export default class App extends Component {
       <ScrollView>
         <View style={styles.container}>
           <Header centerComponent={{ text: 'Garage Solidaire', style: { color: '#fff' }}}/>
+          <View>
+            <CheckBox center title='Monsieur' checked={this.state.checked}/>
+            <CheckBox center title='Madame' checked={this.state.notChecked}/>
+          </View>
           <Input style={styles.firstInput} placeholder='Nom...' leftIcon= {<Icon name='user'size={17}color='#7B8894'/>}/>
           <Input placeholder='Prénom...' leftIcon= {<Icon name='user'size={17}color='#7B8894'/>}/>
           <Input placeholder='Date de naissance...' leftIcon= {<Icon name='calendar'size={17}color='#7B8894'/>}/>
@@ -41,21 +46,33 @@ export default class App extends Component {
           <Input placeholder='Contrat de travail...' leftIcon= {<Icon name='pencil-square-o'size={17}color='#7B8894'/>}/>
           <View>
             <CheckBox center title='QPV' checked={this.state.checked}/>
-            <CheckBox center title='RSA' checked={this.state.checked}/>
+            <CheckBox center title='RSA' checked={this.state.notChecked}/>
           </View>
           <Input placeholder="Carte d'identité..." leftIcon= {<Icon name='address-card'size={17}color='#7B8894'/>}/>
           <Input placeholder='Empreinte banquaire...' leftIcon= {<Icon name='credit-card-alt'size={17}color='#7B8894'/>}/>
-          <Input placeholder='Modèle...' leftIcon= {<Icon name='car'size={17}color='#7B8894'/>}/>
           <Picker
-              style={{ height: 50, width: 150 }}
+              style={{ height: 50, width: 150, marginTop: 10, marginBottom: 10}}
               onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
-              <Picker.Item label="Modèle" value="mode" />
+              <Picker.Item label="Marque" value="mode" />
               <Picker.Item label="Peugeot" value="pt" />
               <Picker.Item label="Renault" value="rt" />
           </Picker>
-          <Input placeholder='Marque...' leftIcon= {<Icon name='star'size={17}color='#7B8894'/>}/>
+          <Picker
+              style={{ height: 50, width: 150, marginTop: 10, marginBottom: 10}}
+              onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+              <Picker.Item label="Modèle" value="mode" />
+              <Picker.Item label="106" value="p106" />
+              <Picker.Item label="206" value="p206" />
+          </Picker>
           <Input placeholder='Immatriculation...' leftIcon= {<Icon name='bars'size={17}color='#7B8894'/>}/>
-          <Input placeholder='Etat Véhicule...' leftIcon= {<Icon name='heartbeat'size={17}color='#7B8894'/>}/>
+          <Picker
+              style={{ height: 50, width: 150, marginTop: 10, marginBottom: 10 }}
+              onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
+              <Picker.Item label="Etat" value="mode" />
+              <Picker.Item label="Bon état" value="bon" />
+              <Picker.Item label="Etat correct" value="correct" />
+              <Picker.Item label="Mauvais état" value="mauvais" />
+          </Picker>
           <Input placeholder='Kilométrage jour...' leftIcon= {<Icon name='tachometer'size={17}color='#7B8894'/>}/>
           <Input placeholder='Date de départ...' leftIcon= {<Icon name='map-o'size={17}color='#7B8894'/>}/>
           <Input placeholder='Date de retour...' leftIcon= {<Icon name='share'size={17}color='#7B8894'/>}/>
